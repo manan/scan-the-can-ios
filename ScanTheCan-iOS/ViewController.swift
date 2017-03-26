186d185c49c8f0d1e0fa8309be941f3085a1a21e
Manan Mehta <mehtamanan@icloud.com>
2017-03-25 23:12:21 -0700

    Initial Commit

---
+++ ScanTheCan-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScanTheCan-iOS
+//
+//  Created by Manan Mehta on 2017-03-25.
+//  Copyright © 2017 mehtamanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
